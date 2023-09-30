99d480655b7d0cc43c3a8b7218b25548befd2090
Roj <ez@roj.im>
2023-09-30 21:37:35 +0300

    Fix Drizzle config

--- api/drizzle/0000_petite_randall_flagg.sql
+++ api/drizzle/0000_petite_randall_flagg.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS "notes" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone,
 	"user_id" bigint NOT NULL,
 	"text" text NOT NULL,
 	CONSTRAINT "notes_id_unique" UNIQUE("id")
